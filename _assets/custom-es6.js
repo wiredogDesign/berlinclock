05c9b57c0b2abcde00e1e8de06e6341286bc9bb6
Tom - MBP <tom@wiredog.co.uk>
2020-10-01 22:11:28 +0100

    It all works now

--- _assets/custom-es6.js
+++ _assets/custom-es6.js
@@ -22,9 +22,24 @@ setInterval(function () {
     var i = 0;
 
     for( i = 0; i < fiveHours; i++ ) { hoursTop[i].classList.add('on'); }
+        if( fiveHours == 0 ) {
+            Array.from(hoursTop).forEach(hoursTop => hoursTop.classList.remove('on'))
+        }
+
     for( i = 0; i < oneHour; i++ ) { hoursBottom[i].classList.add('on'); }
+        if( oneHour == 0 ) {
+            Array.from(hoursBottom).forEach(hoursBottom => hoursBottom.classList.remove('on'))
+        }
+
     for( i = 0; i < fiveMins; i++ ) { minsTop[i].classList.add('on'); }
+        if( fiveMins == 0 ) {
+            Array.from(minsTop).forEach(minsTop => minsTop.classList.remove('on'))
+        }
+
     for( i = 0; i < oneMin; i++ ) { minsBottom[i].classList.add('on'); }
+        if( oneMin == 0 ) {
+            Array.from(minsBottom).forEach(minsBottom => minsBottom.classList.remove('on'))
+        }
 
     if(hours <= 8 || hours >= 20) {
         document.querySelector("body").style.backgroundBlendMode = "hard-light";
