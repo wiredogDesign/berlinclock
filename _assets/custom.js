99422e0a1314bb67353cbd7444cbdf85a4be6352
Tom - MBP <wiredogDesign@icloud.com>
2021-11-03 00:14:22 +0000

    Changed if statements to ES6 notation

--- _assets/custom.js
+++ _assets/custom.js
@@ -9,11 +9,6 @@ setInterval(() => {
     // makes month two digits if less than 10
     var monthCorrected = month < 10 ? '0' + month : '' + month;
     var dayCorrected = day < 10 ? '0' + day : '' + day;
-    // var ymd = year + "*" + monthCorrected + "*" + day;
-    // display date in header
-    document.querySelector('.y').textContent = year;
-    document.querySelector('.m').textContent = monthCorrected;
-    document.querySelector('.d').textContent = dayCorrected;
     
     var hoursX5 = Math.floor(hours / 5); // gives quotient & discards remainder
     var hoursX1 = Math.floor(hours % 5); // gives remainder & discards quotient
@@ -34,35 +29,25 @@ setInterval(() => {
     // eg quotient at 10:00 is 2
     // so only first two blocks would be on
     // then remove the <on> class when the count reaches 0
-    for(i = 0; i < hoursX5; i++) {
-        hx5[i].classList.add('on')
-    }
-    if(hoursX5 == 0) {
-        Array.from(hx5).forEach(hx5 => hx5.classList.remove('on'))
-    }
-    //////////////////////////////////////////////////////////////
+    for(i = 0; i < hoursX5; i++) { hx5[i].classList.add('on') }
+    hoursX5 == 0 ? Array.from(hx5).forEach(hx5 => hx5.classList.remove('on')) : ""
+    /////////////////////////////////////////////////////////////////////////////
 
-    for(i = 0; i < hoursX1; i++ ) {
-        hx1[i].classList.add('on')
-    }
-    if( hoursX1 == 0) {
-        Array.from(hx1).forEach(hx1 => hx1.classList.remove('on'))
-    }
-    //////////////////////////////////////////////////////////////
+    for(i = 0; i < hoursX1; i++ ) { hx1[i].classList.add('on') }
+    hoursX1 == 0 ? Array.from(hx1).forEach(hx1 => hx1.classList.remove('on')) : ""
+    /////////////////////////////////////////////////////////////////////////////
 
-    for(i = 0; i < minsX5; i++ ) {
-        mx5[i].classList.add('on')
-    }
-    if( minsX5 == 0) {
-        Array.from(mx5).forEach(mx5 => mx5.classList.remove('on'))
-    }
-    /////////////////////////////////////////////////////////////// 
+    for(i = 0; i < minsX5; i++ ) { mx5[i].classList.add('on') }
+    minsX5 == 0 ?  Array.from(mx5).forEach(mx5 => mx5.classList.remove('on')) : ""
+    /////////////////////////////////////////////////////////////////////////////
 
-    for(i = 0; i < minsX1; i++ ) {
-        mx1[i].classList.add('on')
-    }
-    if(minsX1 == 0) {
-        Array.from(mx1).forEach(mx1 => mx1.classList.remove('on'))
-    }
+    for(i = 0; i < minsX1; i++ ) { mx1[i].classList.add('on') }
+    minsX1 == 0 ? Array.from(mx1).forEach(mx1 => mx1.classList.remove('on')) : ""
+    ////////////////////////////////////////////////////////////////////////////
+
+    // display date in header
+    document.querySelector('.y').textContent = year;
+    document.querySelector('.m').textContent = monthCorrected;
+    document.querySelector('.d').textContent = dayCorrected;
     
 }, 1000);
